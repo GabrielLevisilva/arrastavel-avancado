76c42d315ae57d8a71b44eb409081b8b308b6fc2
Gabriel Levi <137122494+GabrielLevisilva@users.noreply.github.com>
2023-10-07 14:45:18 -0300

    v2 Resizer funcionando

--- js/eventosResizers.js
+++ js/eventosResizers.js
@@ -1,14 +1,34 @@
 // eventosResizers.js
+let initialX, initialY, currentX, currentY;
+
 const aumentarX = (elementoResizerX, elementoClicado) => {
     if (elementoClicado.getAttribute('data-redimensionando') === 'true') {
         // Se o atributo data-redimensionando for true, então estamos redimensionando este elemento
-        elementoResizerX.style.backgroundColor = 'pink';
-        let cursorX = event.pageX;
-        elementoResizerX.style.left = cursorX - 25 + 'px';
-        let elementoWidth = elementoClicado.getBoundingClientRect().width;
-        let espacoEntre = cursorX - elementoWidth;
-        elementoClicado.style.width = (elementoWidth + espacoEntre ) - 25 + 'px';
-        elementoClicado.style.height = (elementoWidth + espacoEntre ) - 25 + 'px';
+        currentX = event.clientX;
+        currentY = event.clientY;
+
+        const width = parseInt(getComputedStyle(elementoClicado, null).getPropertyValue('width'));
+        const height = parseInt(getComputedStyle(elementoClicado, null).getPropertyValue('height'));
+
+        const deltaX = currentX - initialX;
+        const deltaY = currentY - initialY;
+
+        elementoClicado.style.width = width + deltaX + 'px';
+        elementoClicado.style.height = width + deltaX + 'px';
+
+        initialX = currentX;
+        initialY = currentY;
+
+
+        // elementoResizerX.style.backgroundColor = 'pink';
+        // let cursorX = event.pageX;
+        // elementoResizerX.style.left = cursorX - 25 + 'px';
+        // let elementoWidth = elementoClicado.getBoundingClientRect().width;
+        // let espacoEntre = cursorX - elementoWidth;
+        // let elementoLeft = elementoClicado.getBoundingClientRect().left;
+        // console.log('ele está à ' + elementoLeft + 'px de distancia da Esquerda')
+        // elementoClicado.style.width = (elementoWidth + espacoEntre ) - 25 + 'px';
+        // elementoClicado.style.height = (elementoWidth + espacoEntre ) - 25 + 'px';
     }
 };
 
@@ -18,11 +38,15 @@ const addEventosResizers = (elementoClicado) => {
 
     elementoResizerX.addEventListener('mousedown', (event) => {
         event.stopPropagation();
+
+        initialX = event.clientX;
+        initialY = event.clientY;
         
         elementoClicado.setAttribute('data-redimensionando', 'true');
         
         document.addEventListener('mousemove', () => {
             aumentarX(elementoResizerX, elementoClicado);
+            
         });
         document.addEventListener('mouseup', () => {
             
